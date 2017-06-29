f9fde4a6fe8a23f59f43bbd20f5919257e146625
mickyginger <mickyginger@gmail.com>
2017-06-29 16:25:04 +0100

    Adding directive

--- src/mh-dragdrop-image.directive.js
+++ src/mh-dragdrop-image.directive.js
@@ -2,6 +2,42 @@ angular
   .module('mhDragdropImage')
   .directive('dragDropImg', dragDropImg);
 
-function dragDropImg() {
+dragDropImg.inject = ['$window'];
+function dragDropImg($window) {
+  const fileReader = new $window.FileReader();
 
+  return {
+    restrict: 'E',
+    require: 'ngModel',
+    template: '<div class="drag-drop-img" ng-class="{ active: active }"><img ng-src="{{ ngModel }}"></div>',
+    scope: {
+      active: '='
+    },
+    link($scope, $element, attrs, ngModel) {
+
+      $scope.active = false;
+
+      fileReader.onload = function() {
+        ngModel.$setViewValue(this.result);
+      };
+
+      $element
+        .on('dragenter', () => {
+          $scope.active = true;
+          $scope.$apply();
+        })
+        .on('dragover', (e) => {
+          e.preventDefault();
+        })
+        .on('drageleave', () => {
+          $scope.active = false;
+          $scope.$apply();
+        })
+        .on('drop', (e) => {
+          e.preventDefault();
+          const file = (e.target.files || e.dataTransfer.files)[0];
+          fileReader.readAsDataURL(file);
+        });
+    }
+  };
 }
--- src/mh-dragdrop-image.directive_test.js
+++ src/mh-dragdrop-image.directive_test.js
@@ -1,35 +1,5 @@
-/* global describe, beforeEach, it, expect, inject */
-describe('ngCopyText module', function () {
-  beforeEach(module('ngCopyText'));
-  beforeEach(function () {
-    this.addMatchers({
-      toHaveClass: function (cls) {
-        this.message = function () {
-          return 'Expected \'' + angular.mock.dump(this.actual) + '\' to have class \'' + cls + '\'.';
-        };
+/* global describe, beforeEach */
+describe('mhDragdropImage module', function () {
+  beforeEach(module('mhDragdropImage'));
 
-        return this.actual.hasClass(cls);
-      }
-    });
-    this.addMatchers({
-      toHaveFocus: function () {
-        this.message = function () {
-          return 'Expected \'' + angular.mock.dump(this.actual) + '\' to have focus';
-        };
-
-        return document.activeElement === this.actual[0];
-      }
-    });
-  });
-
-  describe('copy-text directive', function () {
-    it('should print current version', function () {
-      inject(function ($compile, $rootScope) {
-        var element = angular.element($compile('<span data-copy-text>TEXT TO COPY</span>')($rootScope));
-        element.triggerHandler('click');
-        expect(element.text()).toEqual('TEXT TO COPY');
-        expect(element).toHaveClass('ng-isolate-scope');
-      });
-    });
-  });
 });
