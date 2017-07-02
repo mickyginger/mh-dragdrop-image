339415dff11b6059ef53ebd0b7dbf9cc35734a39
mickyginger <mickyginger@gmail.com>
2017-07-03 00:07:51 +0100

    Adding comments to ruby code

--- server/ruby.rb
+++ server/ruby.rb
@@ -1,6 +1,7 @@
-require 'securerandom'
-require 'base64'
+require 'securerandom' # https://ruby-doc.org/stdlib-2.1.2/libdoc/securerandom/rdoc/SecureRandom.html
+require 'base64' # https://ruby-doc.org/stdlib-2.1.3/libdoc/base64/rdoc/Base64.html
 
+# mime type whitelist
 WHITELIST = [
   'image/jpeg',
   'image/png',
@@ -8,14 +9,20 @@ WHITELIST = [
 ];
 
 def upload_image(data_uri)
+  # split the dataURI into its constituent parts
   parts = data_uri.match(/^data:(.*);base64,(.*)$/)
 
+  # check that mime type is in whitelist
   raise "Invalid file" unless WHITELIST.contains parts[1]
 
+  # decode the data
   image = Base64.decode64 parts[3]
+  # generate a reasonable file extension from the mime type
   ext = MIME::Types[parts[1]].first.preferred_extension
+  # create the filename
   filename = "#{SecureRandom.uuid}.#{ext}"
 
+  # create the file in a tmp folder
   File.open("tmp/#{filename}", "w") do |file|
     file.write(image)
     puts "File successfully uploaded"
